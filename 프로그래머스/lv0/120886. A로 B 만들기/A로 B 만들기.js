dc9757e04fc02aeadd472221adcccc8d8bfe9010
moondrop0816 <87507011+moondrop0816@users.noreply.github.com>
2023-02-19 19:22:17 +0900

    [level 0] Title: A로 B 만들기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,6 +1,4 @@
 function solution(before, after) {
-  before = before.split('').sort().join('');
-  after = after.split('').sort().join('');
 
-  return before === after ? 1 : 0
+  return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0
 }
