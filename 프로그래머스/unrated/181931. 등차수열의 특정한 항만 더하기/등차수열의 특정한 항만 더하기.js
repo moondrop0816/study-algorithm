3f41d010fd4c31c7f1320900523a393aebb8e59a
moondrop0816 <87507011+moondrop0816@users.noreply.github.com>
2023-07-05 19:40:21 +0900

    [unrated] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,7 @@
+function solution(a, d, included) {
+    let result = 0;
+    for (let i = 0; i <= included.length; i++) {
+        included[i] ? result += a + (d * i) : 0
+    }
+    return result;
+}
