66a9011272a8db2300064d657d4c19c735d44e8a
Harry <87507011+moondrop0816@users.noreply.github.com>
2023-11-27 23:58:01 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,5 @@
+function solution(my_string) {
+  const str = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P','Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  return str.map((el)=>my_string.split(el).length - 1)
+}
