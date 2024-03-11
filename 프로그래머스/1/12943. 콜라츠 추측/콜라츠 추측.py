c4b8d1a2001789adb987afaa5194ef372b3f34a3
Harry <87507011+moondrop0816@users.noreply.github.com>
2024-03-11 17:45:40 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,18 @@
+def solution(num):
+    if num == 1:
+        return 0
+    
+    count = 0
+    
+    while count <= 500:
+        if num % 2 == 0:
+            num /= 2
+        else:
+            num = num * 3 + 1
+            
+        count+=1
+            
+        if num == 1:
+            break
+            
+    return count if count < 500 else -1
