5c8b306d13c4cd5fd9299d950cdd98e3b4ecf129
hyewon <87507011+moondrop0816@users.noreply.github.com>
2024-03-30 17:08:09 +0900

    [Silver I] Title: IOIOI, Time: 384 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,10 +1,16 @@
 n = int(input())
 m = int(input())
 s = input()
-target = 'I'+'I'.join('O' * n)+'I'
-cnt = 0
-for i in range(m):
-    if s[i:i+len(target)] == target:
-        cnt += 1
+count, idx, result = 0, 0, 0
+while idx < (m-1):
+    if s[idx:idx+3] == 'IOI':
+        idx += 2
+        count += 1
+        if count == n:
+            result += 1
+            count -=1
+    else:
+        idx += 1
+        count = 0
 
-print(cnt)
+print(result)
