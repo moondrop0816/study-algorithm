b6809064217eba79372c1122e3959729d30ca60a
hyewon <87507011+moondrop0816@users.noreply.github.com>
2024-04-02 21:48:47 +0900

    [Gold V] Title: IPv6, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/3107. IPv6/IPv6.py
@@ -0,0 +1,15 @@
+ipv6 = input()
+ipv6 = ipv6.split(':')
+if ipv6[0] == '':
+    ipv6 = ipv6[1:]
+if ipv6[-1] == '':
+    ipv6 = ipv6[:-1]
+
+result = ''
+for i in ipv6:
+    if i=='':
+        result += '0000:'*(8-len(ipv6)+1)
+    else:
+        result += i.zfill(4)+':'
+
+print(result[:-1])
