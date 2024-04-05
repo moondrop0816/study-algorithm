136c4ed73ae8c9445f2209901e0008e8309e0a5e
hyewon <87507011+moondrop0816@users.noreply.github.com>
2024-04-05 15:54:07 +0900

    [Silver II] Title: 좌표 압축, Time: 1620 ms, Memory: 201748 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,14 +1,7 @@
 n = int(input())
 numbers = list(map(int, input().split()))
 sort_nums = sorted(set(numbers))
-dict = {} 
-idx = 0
-for i in sort_nums:
-    if i in dict:
-        pass
-    else:
-        dict[i] = idx
-        idx += 1
+dict = {sort_nums[i] : i for i in range(len(sort_nums))} 
 for j in range(len(numbers)):
     numbers[j] = dict[numbers[j]]
 print(' '.join(map(str, numbers)))
