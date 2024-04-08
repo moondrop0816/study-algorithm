d75108f9bbd4dca303a3d91ca47c9016817bcaae
hyewon <87507011+moondrop0816@users.noreply.github.com>
2024-04-08 22:38:16 +0900

    [Gold V] Title: 적록색약, Time: 88 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,46 +1,41 @@
 from collections import deque
-
-def BFS(x,y):
-    q.append((x,y))
-    dx = [-1,0,1,0]
-    dy = [0,1,0,-1]
+def bfs(x,y):
+    q.append((x, y))
+    dx = [-1, 0, 1, 0]
+    dy = [0, 1, 0, -1]
     visited[x][y] = 1
     while q:
         x, y = q.popleft()
         for d in range(4):
             nx = x + dx[d]
             ny = y + dy[d]
-            # 인덱스 범위 안에 있으면서, 같은 색이면서, 방문 안한 경우
-            if 0<=nx<N and 0<=ny<N and a[nx][ny] == a[x][y] and not visited[nx][ny]:
-                visited[nx][ny] = 1  # 방문체크 후 큐에 넣음
-                q.append((nx,ny))
+            if 0 <= nx < n and 0 <= ny < n and a[nx][ny] == a[x][y] and not visited[nx][ny]:
+                visited[nx][ny] = 1
+                q.append((nx, ny))
 
-
-N = int(input())
-a = [list(input()) for _ in range(N)]
+n = int(input())
+a = [list(input()) for _ in range(n)]
 q = deque()
 
-# 적록색약 아닌 경우
-visited = [[0] * N for _ in range(N)]
+visited = [[0] * n for _ in range(n)]
 cnt1 = 0
-for i in range(N):
-    for j in range(N):
-        if not visited[i][j]:  # 아직 방문 안한 경우만 체킹
-            BFS(i,j)
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(i, j)
             cnt1 += 1
 
-# 적록색약인 경우
-for i in range(N):
-    for j in range(N):
+for i in range(n):
+    for j in range(n):
         if a[i][j] == 'G':
             a[i][j] = 'R'
 
-visited = [[0] * N for _ in range(N)]
+visited = [[0] * n for _ in range(n)]
 cnt2 = 0
-for i in range(N):
-    for j in range(N):
+for i in range(n):
+    for j in range(n):
         if not visited[i][j]:
-            BFS(i,j)
+            bfs(i, j)
             cnt2 += 1
 
 print(cnt1, cnt2)
