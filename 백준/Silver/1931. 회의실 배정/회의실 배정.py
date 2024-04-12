fbb4c736541de639d4ee1494cae398311d22aa19
hyewon <87507011+moondrop0816@users.noreply.github.com>
2024-04-12 14:26:55 +0900

    [Silver I] Title: 회의실 배정, Time: 4136 ms, Memory: 51900 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,11 @@
+n = int(input())
+meeting = sorted((tuple(map(int, input().split())) for _ in range(n)), key=lambda x: (x[1], x[0]))
+cnt = 1
+endtime = meeting[0][1]
+for i in range(1, n):
+    if meeting[i][0] >= endtime:
+        cnt += 1
+        endtime = meeting[i][1]
+
+print(cnt)
+
