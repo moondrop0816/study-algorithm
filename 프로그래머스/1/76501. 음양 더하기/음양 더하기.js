8586fb7528bd90df17aadda255ecc818ca697d52
hyewon <87507011+moondrop0816@users.noreply.github.com>
2025-03-04 17:59:33 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,9 @@
+function solution(absolutes, signs) {
+  return absolutes.reduce((acc,cur, idx)=>{
+    if (signs[idx]) {
+      return acc += cur
+    } else {
+      return acc -= cur
+    }
+  }, 0)
+}
